599a011373c14e68a54b44f50db02c7bbc210e85
João Pedro Areias de Moraes <joaoareiasmoraes@gmail.com>
2022-12-06 16:28:58 -0300

    Intentionally breaks the project so students can fix it

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -10,10 +10,26 @@ function Footer(props) {
   const [addActivityDialog, setAddActivityDialog] = useState(false);
   const [removeActivity, setRemoveActivity] = props.removeActivities;
 
+  function openActivityDialog() {
+    /*
+    *   This function is called when the user clicks on the add activity button
+    *   It opens the add activity dialog
+    */
+    setAddActivityDialog(true);
+  }
+
+  function closeActivityDialog() {
+    /*
+    *   This function is called when the user clicks on the add activity button
+    *   It closes the add activity dialog
+    */
+    setAddActivityDialog(false);
+  }
+
   return (
     <>
-    <Dialog open={addActivityDialog} onClose={() => setAddActivityDialog(false)} maxWidth={false}>
-      <AddActivityCard close={() => setAddActivityDialog(false)} user={props.user}/>
+    <Dialog open={addActivityDialog} onClose={/*  THE CAT ERASED THIS LINE */} maxWidth={false}>
+      <AddActivityCard close={/*  THE CAT ERASED THIS LINE */} user={props.user}/>
     </Dialog>
 
     <Paper sx={{ 
@@ -22,7 +38,8 @@ function Footer(props) {
     }}>
         <Grid container justifyContent="center">
             <Grid item>
-                <AddActivityButtom onClick={() => setAddActivityDialog(true)}/> &nbsp;
+                
+                <AddActivityButtom onClick={/*  THE CAT ERASED THIS LINE */}/> &nbsp;
                 <RemoveActivityButtom onClick={() => setRemoveActivity(!removeActivity)}/>
             </Grid> 
         </Grid>
